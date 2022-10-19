2b7919e73a545d9952619930e873d839de39a16f
Michael Neil <mneil@mneilsworld.com>
2022-10-19 11:15:10 -0700

    Handle case where module type definitions are unresolved
    Tried this on a new package and found that the ts compiler was
    unable to correctly resolve the type paths. I had to add a fallback
    to resolve the module directly.

--- tests/fixture/src/index.ts
+++ tests/fixture/src/index.ts
@@ -1,4 +1,5 @@
 // straight export from 3rd party dependency
 export * as kms from "aws-cdk-lib/aws-kms";
+export * as constructs from "constructs";
 // overloaded export from 3rd party dependency
 export { default as sqs } from "./sqs";
--- tests/index.test.js
+++ tests/index.test.js
@@ -21,23 +21,25 @@ describe("obvi bundle", () => {
         return done(new Error(stats.toString()));
       }
 
-      // 3. Map asset objects to output filenames
-      const files = stats.toJson().assets.map((x) => x.name);
-      const allFiles = files.every((file) => ["main.js", "sqs.d.ts", "index.d.ts"].includes(file));
-      assert.ok(allFiles);
+      try {
+        // 3. Map asset objects to output filenames
+        const files = stats.toJson().assets.map((x) => x.name);
+        const allFiles = files.every((file) => ["main.js", "sqs.d.ts", "index.d.ts"].includes(file));
+        assert.ok(allFiles);
 
-      const index = fs.readFileSync("./dist/index.d.ts", "utf8");
-      assert.ok(
-        index ===
-          `export * as kms from "./obvi-types/aws-cdk-lib/aws-kms";
+        const index = fs.readFileSync("./dist/index.d.ts", "utf8");
+        assert.ok(
+          index ===
+            `export * as kms from "./obvi-types/aws-cdk-lib/aws-kms";
+export * as constructs from "./obvi-types/constructs";
 export { default as sqs } from "./sqs";
 `
-      );
+        );
 
-      const sqs = fs.readFileSync("./dist/sqs.d.ts", "utf8");
-      assert.ok(
-        sqs ===
-          `import * as sqs from "./obvi-types/aws-cdk-lib/aws-sqs";
+        const sqs = fs.readFileSync("./dist/sqs.d.ts", "utf8");
+        assert.ok(
+          sqs ===
+            `import * as sqs from "./obvi-types/aws-cdk-lib/aws-sqs";
 declare class Queue extends sqs.Queue {
 }
 declare const _default: typeof sqs & {
@@ -45,7 +47,10 @@ declare const _default: typeof sqs & {
 };
 export default _default;
 `
-      );
+        );
+      } catch (e) {
+        return done(e);
+      }
 
       done();
     });
